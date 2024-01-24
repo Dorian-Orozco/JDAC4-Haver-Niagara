34c3fbe277095d565cc30ce607686c5c17771508
Dorian Orozco <146119349+Dorian-Orozco@users.noreply.github.com>
2024-01-23 22:57:04 -0500

    Add project files.

---
+++ Haver Niagara/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Haver_Niagara.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
