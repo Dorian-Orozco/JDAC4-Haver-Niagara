415557c22dc08a36d7059d916511f0c55dfbaba8
Dorian Orozco <146119349+Dorian-Orozco@users.noreply.github.com>
2024-02-04 23:53:26 -0500

    name / sort fixes

--- Haver Niagara/Models/PurchasingDecision.cs
+++ Haver Niagara/Models/PurchasingDecision.cs
@@ -1,10 +1,16 @@
-﻿namespace Haver_Niagara.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Haver_Niagara.Models
 {
     public enum PurchasingDecision
     {
+        [Display(Name ="Rework In House")]
         ReworkInHouse,
+        [Display(Name ="Scrap In House")]
         ScrapInHouse,
+        [Display(Name ="Defer To Engineering")]
         DeferToEngineering,
+        [Display(Name ="Return To Supplier")]
         ReturnToSupplier
     }
 }
