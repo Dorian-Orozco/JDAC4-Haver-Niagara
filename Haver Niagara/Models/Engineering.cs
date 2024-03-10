a2c10be984ab51a3391f80ce05aa3b7fa8120631
Dorian Orozco <146119349+Dorian-Orozco@users.noreply.github.com>
2024-03-09 23:24:31 -0500

    added validation to edit

--- Haver Niagara/Models/Engineering.cs
+++ Haver Niagara/Models/Engineering.cs
@@ -3,16 +3,18 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Haver_Niagara.Models
 {
-    public class Engineering
+    public class Engineering : IValidatableObject
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
 
+        [Required(ErrorMessage = "Please Enter an Engineer's Name")]
         [Display(Name = "Engineering Name")]
         public string Name { get; set; }
 
 
+        [Required(ErrorMessage = "Date is Required")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime Date { get; set; }
@@ -20,12 +22,13 @@ namespace Haver_Niagara.Models
         [Display(Name = "Notify Customer Required?")]
         public bool CustomerNotify { get; set; }
 
-
+        [Required(ErrorMessage = "Select if Drawing Requires Updating")]
         [Display(Name = "Drawing Updated?")]
         public bool DrawUpdate { get; set; }
 
 
-        // "eng_notes" in data model...unsure of.
+        
+        [Required(ErrorMessage = "Engineer's Disposition Notes Required")]
         [Display(Name = "Disposition Sequence")]
         public string DispositionNotes { get; set; }
 
@@ -45,8 +48,18 @@ namespace Haver_Niagara.Models
 
 
         [Display(Name = "Engineering Disposition")]
+        [Required(ErrorMessage = "Please Select a Decision")]
         public EngineeringDisposition EngineeringDisposition { get; set; }
         //Link to NCR 
         public NCR NCR { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var TodaysDate = DateTime.Today;
+            if (Date > TodaysDate || RevisionDate > TodaysDate)
+            {
+                yield return new ValidationResult("Date Cannot be in The Future", new[] { "Date", "RevisionDate"});
+            }
+        }
     }
 }
