3c20920468c0e5f0c010e9982878f6e84fb400c9
Doaa Awan <146845118+DoaaAwan@users.noreply.github.com>
2024-04-06 23:38:35 -0400

    Update NCR.cs

--- Haver Niagara/Models/NCR.cs
+++ Haver Niagara/Models/NCR.cs
@@ -14,6 +14,7 @@ namespace Haver_Niagara.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
+
         public NCR(HaverNiagaraDbContext context)
         {
             _context = context;
@@ -23,16 +24,9 @@ namespace Haver_Niagara.Models
         public string FormattedID
         {
             get
-            { //if year of the ncr when it was created is not equal to the current year, 
-                //if (NCR_Date.Year != DateTime.Now.Year)
-                //{
-                    ////gets the ncrs the year is the same and orders them by id and then turns them into a list
-                    //var ncrsForYear = _context.NCRs.Where(a => a.NCR_Date.Year == NCR_Date.Year).OrderBy(a => a.ID).ToList();
-                    ////gets the index position of the list returned and then adds 1 bc lists start at 0.
-                    //int index = ncrsForYear.FindIndex(a => a.ID == this.ID) + 1;
-                    //return $"{NCR_Date.Year}-{index.ToString().PadLeft(3, '0')}";
-                    //gets the ncrs the year is the same and orders them by id and then turns them into a list
-                    if(_context != null){
+            { 
+                if (_context != null)
+                {
                     var ncrsForYear = _context.NCRs.Where(a => a.NCR_Date.Year == NCR_Date.Year).OrderBy(a => a.ID).ToList();
                     //gets the index position of the list returned and then adds 1 bc lists start at 0.
                     int index = ncrsForYear.FindIndex(a => a.ID == this.ID) + 1;
@@ -40,20 +34,10 @@ namespace Haver_Niagara.Models
                 }
                 else
                 {
-                    return "Issue Occured, Contact Admin";
+                    return "Error: NCR ID could not be generated";
                 }
-
-                //}
-                //return $"{NCR_Date.Year}-{ID.ToString().PadLeft(3, '0')}";
             }
-            //{
-            //    var ncrsForYear = _context.NCRs.Where(a => a.NCR_Date.Year == NCR_Date.Year).OrderBy(a => a.ID).ToList();
-            //    //gets the index position of the list returned and then adds 1 bc lists start at 0.
-            //    int index = ncrsForYear.FindIndex(a => a.ID == this.ID) + 1;
-            //    return $"{NCR_Date.Year}-{index.ToString().PadLeft(3, '0')}";
-            //}
         }
-
 
         [Display(Name = "Date")]
         [DataType(DataType.Date)]
